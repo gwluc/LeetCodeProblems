52d8ffaa57da257dcab29fd061ab183f0544f729
LucMaillet <luccanadien@gmail.com>
2022-11-26 18:34:29 -0400

    problem 5 done

--- problem5.cpp
+++ problem5.cpp
@@ -4,33 +4,48 @@
 
 using namespace std;
 
+// Stack data structure might be better...
+
 bool isValid(string s) {
 
     unordered_map<char, char> Brackets {{'}', '{'}, {')', '('}, {']', '['}};
-    vector<char> arr; 
+    vector<char> arr;
 
     for (int i=0; i < s.length(); i++) {
 
-        if (Brackets.count(s[1]) > 0) {
-        
-            arr.push_back(s[i]);
+        if (Brackets.count(s[i]) > 0) {
+
+            // std::cout << s[i] << std::endl;        
+            if (arr.size() > 0 && (arr.back() == Brackets[s[i]]))  {
+                arr.pop_back();
+            }
+            else {
+                return false;
+            }
         
         } else {
-        
-            std::cout << s[i] << std::endl;
- 
+            // std::cout << s[i] << std::endl;
+            arr.push_back(s[i]);
         }
     }
-    return true;
 
+    return arr.size() ? false : true;
 }
 
 
 int main() {
 
-    cout << isValid("(){}");
-    cout << isValid(")({}");
-    cout << isValid("{}()");
-    cout << isValid("([)]");
+    cout << isValid("(){}") << endl;
+    cout << isValid(")({}") << endl;
+    cout << isValid("{}()") << endl;
+    cout << isValid("([)]") << endl;
+    cout << isValid("([{}])") << endl;
+    cout << isValid("(") << endl;
+
+    //unordered_map<char, char> Brackets {{'}', '{'}, {')', '('}, {']', '['}};
+    //vector<int> arr1 = {1,2,3,4,55};
+
+    //std::cout << (Brackets['}']) << std::endl;
+    //std::cout << (arr1.back() == 40) << std::endl;
 
 }
